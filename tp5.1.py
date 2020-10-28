6ae36e8d96aa2b26bcdf4491ccee1bbb521f92c8
zoher cheddad <arthasdarky@gmail.com>
2020-10-28 20:46:14 +0100

    first commit

---
+++ tp5.1.py
@@ -0,0 +1,13 @@
+def sum(a,b):   #sum c'est somme en anglais
+    return a + b
+a = int(input("Veuillez la valeur de a : "))
+b = int(input("Veuillez la valeur de b : "))
+myAddition = sum (a,b)
+print("mon addition vaut",myAddition)
+
+def somme(c,d):  
+    return c + d
+c = int(input("Veuillez la valeur de c : "))
+d = int(input("Veuillez la valeur de d : "))
+monAddition = somme (c,d)
+print("mon addition vaut",monAddition)
---
+++ tp5.2.py
@@ -0,0 +1,3 @@
+from math import fabs
+z = int(input("Veuillez écrire un chiffre négative pour z : "))
+print ("la variable z à pour valeur positive",fabs(z))
